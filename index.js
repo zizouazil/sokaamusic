ef5c5c6297c6e71cdcdd96565a402766607d479f
zizouazil <40892963+zizouazil@users.noreply.github.com>
2018-10-05 08:15:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
 const prefix = "M";
+const queue = new Map();
+
 
 
 client.on('ready', () => {
